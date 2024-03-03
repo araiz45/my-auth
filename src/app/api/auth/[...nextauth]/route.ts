22ab83360d362a89509acc886216a4b9cd970c8e
araiz45 <araizzahid45@gmail.com>
2024-03-03 21:48:52 +0500

    final commit

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,20 @@
+// iMPORTS
+import NextAuth from "next-auth/next";
+// import from providers
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
